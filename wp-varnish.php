06dde36dd1f5676f23abe399dfa5663417685ef4
Amaury Balmer <amaury@beapi.fr>
2014-01-08 11:11:46 +0100

    Kill direct call on plugin file

--- wp-varnish.php
+++ wp-varnish.php
@@ -22,6 +22,11 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit();
+}
+
 class WPVarnish {
 
 	public $wpv_addr_optname;
